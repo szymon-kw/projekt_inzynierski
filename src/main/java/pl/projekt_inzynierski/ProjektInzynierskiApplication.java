0f4e67fbf4e716ccb3a6df1abf7ee5de083ea9e3
szymon-kw <szymonkwiecien7@wp.pl>
2024-10-16 19:34:37 +0200

    Initial commit

---
+++ src/main/java/pl/projekt_inzynierski/ProjektInzynierskiApplication.java
@@ -0,0 +1,13 @@
+package pl.projekt_inzynierski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektInzynierskiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektInzynierskiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/projekt_inzynierski/ProjektInzynierskiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekt_inzynierski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektInzynierskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4de565cfa5d3b3ecf4e2873eb6247f52c59ad85c
Mateusz <mateuszprucia@outlook.com>
2024-12-02 20:10:17 +0100

    + zarzadzanie firmami

--- src/main/java/pl/projekt_inzynierski/company/CompanyRepository.java
+++ src/main/java/pl/projekt_inzynierski/company/CompanyRepository.java
@@ -1,6 +1,11 @@
 package pl.projekt_inzynierski.company;
+import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByName(String name);
 }
+
+
